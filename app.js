7f5fc59fb5b42cc60d8b9eb2a2cb03daedf89af6
essleeung <essleeung@gmail.com>
2020-04-09 21:11:54 -0400

    finished hwk

--- app.js
+++ app.js
@@ -5,3 +5,25 @@
 // 4. Get another password from the user
 // 5. If the second password matches the first one, print a success message
 // 6. If the second password does not match the first one, print 'access denied'
+
+var readlineSync = require('readline-sync');
+var bcrypt = require('bcryptjs');
+var saltRounds = 10;
+// Wait for user's response.
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+
+// Handle the secret text (e.g. password).
+var password = readlineSync.question('What is your password? ', {
+    hideEchoBack: true // The typed text on screen is hidden by `*` (default).
+  })
+var hashedPassword = bcrypt.hashSync(password, saltRounds);
+var confirmPassword = readlineSync.question('Please confirm your password', {
+    hideEchoBack: true 
+})
+var result = bcrypt.compareSync(confirmPassword, hashedPassword)
+if (result === true) {
+    console.log ('passwords were a match!')
+} else {
+    console.log ('passwords do NOT match! please try again!')
+}
